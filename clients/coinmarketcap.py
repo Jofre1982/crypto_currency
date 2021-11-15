ced09c5f98662bcc34bd659ebcc7fa3b4b30d36f
Jofre <jofre1706@gmail.com>
2021-11-15 12:33:55 -0500

    cliente de servicio rest

---
+++ clients/coinmarketcap.py
@@ -0,0 +1,6 @@
+base_url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/"
+headers = {'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY':  '760d2fba-ffcf-4172-9348-c8ca0afebc52'}
+
+def convert(to='BTC'):
+    return 0
